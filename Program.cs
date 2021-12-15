d62b6dbf72e91a39ce812545ab62cefc46f11201
Malmsteg <73699667+Malmsteg@users.noreply.github.com>
2021-12-15 09:57:57 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace AdventOfCode_2021_Day15
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string file = Path.Combine(currentDirectory, @"..\..\..\test.txt");
+            string path = Path.GetFullPath(file);
+            string[] text = File.ReadAllText(path).Replace("\r", "").Split("\n");
+
+            int xLen = text[0].Length;
+            int yLen = text.Length;
+
+            int[,] puzzleInput = new int[xLen,yLen];
+
+            for(int i = 0; i < text[0].Length; i++)
+            {
+                for (int j = 0; j < text.Length; j++)
+                {
+                    puzzleInput[i,j] = Convert.ToInt32(text[i][j]);
+                }
+            }
+
+            int aPath = 0;
+            for(int i = 1; i < puzzleInput.GetLength(0);i++)
+            {
+                aPath+= puzzleInput[i,0];
+            }
+            for(int j = 1; j < puzzleInput.GetLength(1);j++)
+            {
+                aPath+= puzzleInput[0,j];
+            }
+            Console.WriteLine(aPath);
+
+
+        }
+    }
+}
